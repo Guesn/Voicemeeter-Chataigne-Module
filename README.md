0d8adc9372f7554b1cd8263047a657e2bb9c8d5d
Guesn <guesneau@gmail.com>
2023-08-15 15:53:23 +0200

    Adding Callback Methods

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Voicemeeter-Chataigne-Module
 A Chataigne module to be a master of Voicemeeter
+
+Version 1.0.1
+- Adding Callback methods
+
 Version 1.0
+- First iteration
--- Voicemeeter.js
+++ Voicemeeter.js
@@ -4,12 +4,22 @@ function init(){
 
 function moduleValueChanged(value){
     script.log('moduleValueChanged');
+    script.log(value.isParameter());
     canal = value.getParent().name;
-    if (canal == "tape"){tape(value);}
-    else {
-        zone = value.getParent().getParent().name;
-        if (zone == "strips"){strip_update(value);}
-        if (zone == "bus"){bus_update(value);}
+    if (value.isParameter()){
+        if (canal == "tape"){tape(1, value.name, value.get());}
+        else {
+            zone = value.getParent().getParent().name;
+            if (zone == "strips"){strip_update(canal, value.name, value.get());}
+            if (zone == "bus"){bus_update(canal, value.name, value.get());}
+        }
+    } else {
+        if (canal == "tape"){tape(1, value.name, 1);}
+        else {
+            zone = value.getParent().getParent().name;
+            if (zone == "strips"){strip_update(canal, value.name, 1);}
+            if (zone == "bus"){bus_update(canal, value.name, 1);}
+        }
     }
 }
 
@@ -51,155 +61,152 @@ function pitchWheelEvent(channel,value){
     if (channel >= 9) {local.values.bus.getChild(channel-8).gain_Fader.set((value-1)/16383);}
 }
 
-function tape(value){
-    canal = 1;
-    if (value.name == "tape_Gain"){
-        local.sendCC(canal,100,value.get()*127);
+function tape(canal, nom, value){
+    if (nom == "tape_Gain"){
+        local.sendCC(canal,100,parseFloat(value)*127);
     }
-    if (value.name == "play"){
+    if (nom == "play"){
         local.sendCC(canal,101,1);
     }
-    if (value.name == "rec"){
+    if (nom == "rec"){
         local.sendCC(canal,102,1);
     }
-    if (value.name == "stop"){
+    if (nom == "stop"){
         local.sendCC(canal,103,1);
     }
-    if (value.name == "rew"){
+    if (nom == "rew"){
         local.sendCC(canal,104,1);
     }
-    if (value.name == "ff"){
+    if (nom == "ff"){
         local.sendCC(canal,105,1);
     }
-    if (value.name == "replay"){
+    if (nom == "replay"){
         local.sendCC(canal,106,1);
     }
 }
 
-function strip_update(value){
+function strip_update(canal, nom, value){
     script.log('Strip Update');
-    canal = value.getParent().name;
-    if (value.name == "switch_B1_B2"){
+    if (nom == "switch_B1_B2"){
         local.sendCC(canal,0,1);
     }
-    if (value.name == "gain_Fader"){
-        local.sendPitchWheel(canal,value.get()*16383);
+    if (nom == "gain_Fader"){
+        local.sendPitchWheel(canal,parseFloat(value)*16383);
     }
-    if (value.name == "l_R_Pan"){
-        local.sendCC(canal,1,value.get()*127);
+    if (nom == "l_R_Pan"){
+        local.sendCC(canal,1,parseFloat(value)*127);
     }
-    if (value.name == "reset_Pan"){
+    if (nom == "reset_Pan"){
         local.sendCC(canal,2,1);
     }
-    if (value.name == "mute"){
+    if (nom == "mute"){
         local.sendCC(canal,3,1);
     }
-    if (value.name == "solo"){
+    if (nom == "solo"){
         local.sendCC(canal,4,1);
     }
-    if (value.name == "comp"){
-        local.sendCC(canal,5,value.get()*127);
+    if (nom == "comp"){
+        local.sendCC(canal,5,parseFloat(value)*127);
     }
-    if (value.name == "gate"){
-        local.sendCC(canal,6,value.get()*127);
+    if (nom == "gate"){
+        local.sendCC(canal,6,parseFloat(value)*127);
     }
-    if (value.name == "denoiser"){
-        local.sendCC(canal,7,value.get()*127);
+    if (nom == "denoiser"){
+        local.sendCC(canal,7,parseFloat(value)*127);
     }
-    if (value.name == "limiter"){
-        local.sendCC(canal,8,value.get()*127);
+    if (nom == "limiter"){
+        local.sendCC(canal,8,parseFloat(value)*127);
     }
-    if (value.name == "reverb"){
-        local.sendCC(canal,9,value.get()*127);
+    if (nom == "reverb"){
+        local.sendCC(canal,9,parseFloat(value)*127);
     }
-    if (value.name == "delay"){
-        local.sendCC(canal,10,value.get()*127);
+    if (nom == "delay"){
+        local.sendCC(canal,10,parseFloat(value)*127);
     }
-    if (value.name == "ext_FX1"){
-        local.sendCC(canal,11,value.get()*127);
+    if (nom == "ext_FX1"){
+        local.sendCC(canal,11,parseFloat(value)*127);
     }
-    if (value.name == "ext_FX2"){
-        local.sendCC(canal,12,value.get()*127);
+    if (nom == "ext_FX2"){
+        local.sendCC(canal,12,parseFloat(value)*127);
     }
-    if (value.name == "a1"){
+    if (nom == "a1"){
         local.sendCC(canal,13,1);
     }
-    if (value.name == "a2"){
+    if (nom == "a2"){
         local.sendCC(canal,14,1);
     }
-    if (value.name == "a3"){
+    if (nom == "a3"){
         local.sendCC(canal,15,1);
     }
-    if (value.name == "a4"){
+    if (nom == "a4"){
         local.sendCC(canal,16,1);
     }
-    if (value.name == "a5"){
+    if (nom == "a5"){
         local.sendCC(canal,17,1);
     }
-    if (value.name == "b1"){
+    if (nom == "b1"){
         local.sendCC(canal,18,1);
     }
-    if (value.name == "b2"){
+    if (nom == "b2"){
         local.sendCC(canal,19,1);
     }
-    if (value.name == "b3"){
+    if (nom == "b3"){
         local.sendCC(canal,20,1);
     }
-    if (value.name == "m_c"){
+    if (nom == "m_c"){
         local.sendCC(canal,21,1);
     }
-    if (value.name == "k"){
+    if (nom == "k"){
         local.sendCC(canal,21,1);
     }
-    if (value.name == "eQ_High"){
-        local.sendCC(canal,22,value.get()*127);
+    if (nom == "eQ_High"){
+        local.sendCC(canal,22,parseFloat(value)*127);
     }
-    if (value.name == "eQ_Med"){
-        local.sendCC(canal,23,value.get()*127);
+    if (nom == "eQ_Med"){
+        local.sendCC(canal,23,parseFloat(value)*127);
     }
-    if (value.name == "eQ_Low"){
-        local.sendCC(canal,24,value.get()*127);
+    if (nom == "eQ_Low"){
+        local.sendCC(canal,24,parseFloat(value)*127);
     }
-    if (value.name == "app#1_Volume"){
-        local.sendCC(canal,25,value.get()*127);
+    if (nom == "app#1_Volume"){
+        local.sendCC(canal,25,parseFloat(value)*127);
     }
-    if (value.name == "app#2_Volume"){
-        local.sendCC(canal,26,value.get()*127);
+    if (nom == "app#2_Volume"){
+        local.sendCC(canal,26,parseFloat(value)*127);
     }
-    if (value.name == "app#3_Volume"){
-        local.sendCC(canal,27,value.get()*127);
+    if (nom == "app#3_Volume"){
+        local.sendCC(canal,27,parseFloat(value)*127);
     }
-    if (value.name == "app#4_Volume"){
-        local.sendCC(canal,28,value.get()*127);
+    if (nom == "app#4_Volume"){
+        local.sendCC(canal,28,parseFloat(value)*127);
     }
-    if (value.name == "app#1_Mute"){
+    if (nom == "app#1_Mute"){
         local.sendCC(canal,29,1);
     }
-    if (value.name == "app#2_Mute"){
+    if (nom == "app#2_Mute"){
         local.sendCC(canal,30,1);
     }
-    if (value.name == "app#3_Mute"){
+    if (nom == "app#3_Mute"){
         local.sendCC(canal,31,1);
     }
-    if (value.name == "app#4_Mute"){
+    if (nom == "app#4_Mute"){
         local.sendCC(canal,32,1);
     }
 }
 
-function bus_update(value){
+function bus_update(canal, nom, value){
     script.log('Bus Update');
-    canal = value.getParent().name;
     canal = parseInt(canal) + 8;
-    if (value.name == "gain_Fader"){
-        local.sendPitchWheel(canal,value.get()*16383);
+    if (nom == "gain_Fader"){
+        local.sendPitchWheel(canal,parseFloat(value)*16383);
     }
-    if (value.name == "select"){
+    if (nom == "select"){
         local.sendCC(canal,2,1);
     }
-    if (value.name == "mute"){
+    if (nom == "mute"){
         local.sendCC(canal,3,1);
     }
-    if (value.name == "eq"){
+    if (nom == "eq"){
         local.sendCC(canal,4,1);
     }
 }
